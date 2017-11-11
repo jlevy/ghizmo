02ecce1f3b7710fd4cde25620291f76141bd8c49
Joshua Levy <joshua@cal.berkeley.edu>
2017-11-11 14:35:16 -0800

    setup.py fixups for Python 3. assemble-authors update.

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ from ghizmo import main
 setup(
   name="ghizmo",
   version=main.VERSION,
+  python_requires='>=3',
   packages=find_packages(),
   author="Joshua Levy",
   license="Apache 2",
   url="https://github.com/jlevy/ghizmo",
-  install_requires=["github3.py>=1.0.0a4", "PyYAML>=3.12"],
+  # Pinning uritemplate dep; see https://github.com/sigmavirus24/github3.py/issues/634
+  install_requires=["github3.py>=1.0.0a4", "PyYAML>=3.12", "uritemplate.py==2.0.0"],
   description=main.DESCRIPTION,
   long_description=main.LONG_DESCRIPTION,
   classifiers=[
